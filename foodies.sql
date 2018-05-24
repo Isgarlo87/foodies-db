14ca588d034cc49bd91dacb86564c94b64b36369
Kysuke <kysuke87@gmail.com>
2018-05-24 22:39:27 +0200

    Add files via upload

--- foodies.sql
+++ foodies.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 30-04-2018 a las 08:38:39
+-- Tiempo de generación: 01-05-2018 a las 09:42:46
 -- Versión del servidor: 5.6.36
 -- Versión de PHP: 5.6.31
 
@@ -30,10 +30,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `extras` (
   `id` int(11) NOT NULL,
-  `descripcion` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `imagen` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
   `precio` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `extras`
@@ -57,9 +57,9 @@ CREATE TABLE `lineapedido` (
   `id` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL,
   `importe` double NOT NULL,
-  `id_pedido` int(11) NOT NULL,
-  `id_plato` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `id_pedido` int(11) DEFAULT NULL,
+  `id_plato` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `lineapedido`
@@ -80,7 +80,15 @@ INSERT INTO `lineapedido` (`id`, `cantidad`, `importe`, `id_pedido`, `id_plato`)
 (12, 1, 6.95, 12, 10),
 (13, 2, 13, 13, 1),
 (14, 3, 32.849999999999994, 14, 2),
-(15, 1, 6.5, 15, 1);
+(15, 1, 6.5, 15, 1),
+(18, 2, 15.9, 17, 5),
+(19, 9, 44.550000000000004, 17, 7),
+(20, 7, 34.65, 18, 8),
+(21, 4, 31.8, 18, 5),
+(22, 3, 26.849999999999998, 18, 9),
+(23, 5, 39.75, 19, 5),
+(24, 1, 6.5, 19, 1),
+(25, 2, 13, 19, 6);
 
 -- --------------------------------------------------------
 
@@ -91,9 +99,9 @@ INSERT INTO `lineapedido` (`id`, `cantidad`, `importe`, `id_pedido`, `id_plato`)
 CREATE TABLE `pedido` (
   `id` int(11) NOT NULL,
   `fecha_pedido` datetime NOT NULL,
-  `id_tienda` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `id_tienda` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `pedido`
@@ -114,7 +122,10 @@ INSERT INTO `pedido` (`id`, `fecha_pedido`, `id_tienda`, `id_usuario`) VALUES
 (12, '2018-04-29 17:51:00', 3, 19),
 (13, '2018-04-29 17:55:00', 3, 19),
 (14, '2018-04-29 18:03:00', 3, 19),
-(15, '2018-04-29 18:25:00', 1, 16);
+(15, '2018-04-29 18:25:00', 1, 16),
+(17, '2018-05-01 18:16:00', 3, 19),
+(18, '2018-05-01 18:21:00', 3, 19),
+(19, '2018-05-01 18:32:00', 3, 19);
 
 -- --------------------------------------------------------
 
@@ -124,28 +135,28 @@ INSERT INTO `pedido` (`id`, `fecha_pedido`, `id_tienda`, `id_usuario`) VALUES
 
 CREATE TABLE `plato` (
   `id` int(11) NOT NULL,
-  `descripcion` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `imagen` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
   `existencias` int(11) NOT NULL,
   `precio` double NOT NULL,
-  `id_tipoplato` int(11) NOT NULL,
-  `id_extras` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `id_tipoplato` int(11) DEFAULT NULL,
+  `id_extras` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `plato`
 --
 
 INSERT INTO `plato` (`id`, `descripcion`, `imagen`, `existencias`, `precio`, `id_tipoplato`, `id_extras`) VALUES
-(1, 'Hamburguesa de pollo', 'http://www.onlinepersonaltrainer.es/wp-content/uploads/2014/11/Hamburguesas-de-pollo.jpg', 47, 6.5, 1, 1),
-(2, 'Filete de ternera', 'https://img.recetascomidas.com/recetas/640_480/filete-de-ternera-a-la-parrilla.jpg', 32, 10.95, 1, 1),
-(3, 'Salmón ahumado', 'https://www.recetin.com/wp-content/uploads/2011/01/salmon-ahumado.jpg', 20, 12.95, 2, 1),
+(1, 'Hamburguesa de pollo', 'http://www.onlinepersonaltrainer.es/wp-content/uploads/2014/11/Hamburguesas-de-pollo.jpg', 75, 6.5, 1, 1),
+(2, 'Filete de ternera', 'https://img.recetascomidas.com/recetas/640_480/filete-de-ternera-a-la-parrilla.jpg', 50, 10.95, 1, 1),
+(3, 'Salmón ahumado', 'https://www.recetin.com/wp-content/uploads/2011/01/salmon-ahumado.jpg', 35, 12.95, 2, 1),
 (4, 'Bacalao al pilpil', 'https://www.miscosillasdecocina.com/wp-content/uploads/2014/02/bacalao-pilpil-e1491084486535.jpg', 25, 11.5, 2, 1),
-(5, 'Espaguetis a la carbonara', 'http://sevilla.abc.es/gurme//wp-content/uploads/2009/08/670_carbonarainterior_1251742200.jpg', 60, 7.95, 3, 1),
-(6, 'Ñoquis con tomate', 'https://upload.wikimedia.org/wikipedia/commons/a/a3/%C3%91oquis1.jpg', 30, 6.5, 3, 1),
-(7, 'Arroz tres delicias', 'https://unareceta.com/wp-content/uploads/2016/10/arroz-tres-delicias.jpg', 94, 4.95, 4, 1),
-(8, 'Arroz al horno', 'https://i.ytimg.com/vi/5tKKQxlLnRY/maxresdefault.jpg', 87, 4.95, 4, 1),
-(9, 'Ensalada cesar', 'https://lacocinadefrabisa.lavozdegalicia.es/wp-content/uploads/2016/04/ensalada-cesar.jpg', 20, 8.95, 5, 1),
+(5, 'Espaguetis a la carbonara', 'http://sevilla.abc.es/gurme//wp-content/uploads/2009/08/670_carbonarainterior_1251742200.jpg', 47, 7.95, 3, 1),
+(6, 'Ñoquis con tomate', 'https://upload.wikimedia.org/wikipedia/commons/a/a3/%C3%91oquis1.jpg', 28, 6.5, 3, 1),
+(7, 'Arroz tres delicias', 'https://unareceta.com/wp-content/uploads/2016/10/arroz-tres-delicias.jpg', 85, 4.95, 4, 1),
+(8, 'Arroz al horno', 'https://i.ytimg.com/vi/5tKKQxlLnRY/maxresdefault.jpg', 80, 4.95, 4, 1),
+(9, 'Ensalada cesar', 'https://lacocinadefrabisa.lavozdegalicia.es/wp-content/uploads/2016/04/ensalada-cesar.jpg', 67, 8.95, 5, 1),
 (10, 'Ensalada mediterranea', 'https://img-global.cpcdn.com/002_recipes/recipes_16759_v1393347074_receta_foto_00016759/400x400cq70/photo.jpg', 22, 6.95, 5, 1);
 
 -- --------------------------------------------------------
@@ -159,9 +170,9 @@ CREATE TABLE `ticket` (
   `fecha_ticket` datetime NOT NULL,
   `iva` int(11) DEFAULT NULL,
   `importe` double NOT NULL,
-  `descripcion` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `id_pedido` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `descripcion` varchar(255) DEFAULT NULL,
+  `id_pedido` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `ticket`
@@ -182,7 +193,10 @@ INSERT INTO `ticket` (`id`, `fecha_ticket`, `iva`, `importe`, `descripcion`, `id
 (12, '2018-04-29 17:51:00', 21, 6.95, 'Ensalada mediterranea', 12),
 (13, '2018-04-29 17:55:00', 21, 13, 'Hamburguesa de pollo', 13),
 (14, '2018-04-29 18:03:00', 21, 32.849999999999994, 'Filete de ternera', 14),
-(15, '2018-04-29 18:25:00', 21, 6.5, 'Hamburguesa de pollo', 15);
+(15, '2018-04-29 18:25:00', 21, 6.5, 'Hamburguesa de pollo', 15),
+(18, '2018-05-01 18:16:00', 21, 60.45, 'Espaguetis a la carbonara, Arroz tres delicias, ', 17),
+(19, '2018-05-01 18:21:00', 21, 93.3, 'Arroz al horno, Espaguetis a la carbonara, Ensalada cesar', 18),
+(20, '2018-05-01 18:32:00', 21, 59.25, 'Espaguetis a la carbonara (x5), Hamburguesa de pollo, Ñoquis con tomate (x2)', 19);
 
 -- --------------------------------------------------------
 
@@ -192,9 +206,9 @@ INSERT INTO `ticket` (`id`, `fecha_ticket`, `iva`, `importe`, `descripcion`, `id
 
 CREATE TABLE `tienda` (
   `id` int(11) NOT NULL,
-  `descripcion` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `direccion` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `descripcion` varchar(255) DEFAULT NULL,
+  `direccion` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `tienda`
@@ -214,8 +228,8 @@ INSERT INTO `tienda` (`id`, `descripcion`, `direccion`) VALUES
 
 CREATE TABLE `tipoplato` (
   `id` int(11) NOT NULL,
-  `descripcion` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `descripcion` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `tipoplato`
@@ -236,8 +250,8 @@ INSERT INTO `tipoplato` (`id`, `descripcion`) VALUES
 
 CREATE TABLE `tipousuario` (
   `id` int(11) NOT NULL,
-  `descripcion` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `descripcion` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `tipousuario`
@@ -256,22 +270,22 @@ INSERT INTO `tipousuario` (`id`, `descripcion`) VALUES
 
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `primer_apellido` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `segundo_apellido` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `telefono` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `direccion` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `codigo_postal` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `login` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `primer_apellido` varchar(255) NOT NULL,
+  `segundo_apellido` varchar(255) NOT NULL,
+  `telefono` varchar(255) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `codigo_postal` varchar(255) DEFAULT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `encargado` tinyint(1) DEFAULT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
   `validado` tinyint(1) DEFAULT NULL,
   `activo` tinyint(1) DEFAULT NULL,
-  `id_tipousuario` int(11) NOT NULL,
+  `id_tipousuario` int(11) DEFAULT NULL,
   `id_tienda` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `usuario`
@@ -378,12 +392,12 @@ ALTER TABLE `extras`
 -- AUTO_INCREMENT de la tabla `lineapedido`
 --
 ALTER TABLE `lineapedido`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 --
 -- AUTO_INCREMENT de la tabla `pedido`
 --
 ALTER TABLE `pedido`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT de la tabla `plato`
 --
@@ -393,7 +407,7 @@ ALTER TABLE `plato`
 -- AUTO_INCREMENT de la tabla `ticket`
 --
 ALTER TABLE `ticket`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT de la tabla `tienda`
 --
@@ -422,35 +436,35 @@ ALTER TABLE `usuario`
 -- Filtros para la tabla `lineapedido`
 --
 ALTER TABLE `lineapedido`
-  ADD CONSTRAINT `fk_linea_pedido_pedido1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_linea_pedido_plato1` FOREIGN KEY (`id_plato`) REFERENCES `plato` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_linea_pedido_pedido1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_linea_pedido_plato1` FOREIGN KEY (`id_plato`) REFERENCES `plato` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `pedido`
 --
 ALTER TABLE `pedido`
-  ADD CONSTRAINT `fk_pedido_tienda1` FOREIGN KEY (`id_tienda`) REFERENCES `tienda` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_pedido_usuario1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_pedido_tienda1` FOREIGN KEY (`id_tienda`) REFERENCES `tienda` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_pedido_usuario1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `plato`
 --
 ALTER TABLE `plato`
-  ADD CONSTRAINT `fk_plato_extras1` FOREIGN KEY (`id_extras`) REFERENCES `extras` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_plato_tipo_plato1` FOREIGN KEY (`id_tipoplato`) REFERENCES `tipoplato` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_plato_extras1` FOREIGN KEY (`id_extras`) REFERENCES `extras` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_plato_tipo_plato1` FOREIGN KEY (`id_tipoplato`) REFERENCES `tipoplato` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `ticket`
 --
 ALTER TABLE `ticket`
-  ADD CONSTRAINT `fk_ticket_pedido1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_ticket_pedido1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD CONSTRAINT `fk_usuario_tienda1` FOREIGN KEY (`id_tienda`) REFERENCES `tienda` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_usuario_tipousuario1` FOREIGN KEY (`id_tipousuario`) REFERENCES `tipousuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_usuario_tienda1` FOREIGN KEY (`id_tienda`) REFERENCES `tienda` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_usuario_tipousuario1` FOREIGN KEY (`id_tipousuario`) REFERENCES `tipousuario` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
